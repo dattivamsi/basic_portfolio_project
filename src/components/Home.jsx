29fea04dfcf008b51630cd404805861985b6acf7
dattivamsi <dattitarun@gmail.com>
2023-08-15 15:13:29 +0530

    minor changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {AiFillLinkedin,AiFillFacebook,AiFillTwitterCircle} from 'react-icons/ai'
 import './portfolio.css'
 
 function Home() {
@@ -16,6 +17,11 @@ function Home() {
       <div className="home-page-button-container">
         <button>Hire Me</button>
         <button>See Projects</button>
+      </div>
+      <div className="icons-container">
+        <button className="icons-button"><span><AiFillLinkedin className="icon" /></span></button>
+        <button className="icons-button"><span><AiFillFacebook className="icon" /></span></button>
+        <button className="icons-button"><span><AiFillTwitterCircle className="icon" /></span></button>
       </div>
     </div>
   );
